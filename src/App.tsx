b418ab2f85da0e27ea8cf477f29b6776873baaf1
litophoto <ciro17581@gmail.com>
2023-11-09 21:12:44 +0900

    create: Create samples main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,53 @@
-import React from "react";
+import { useState } from 'react'
+import {
+  CSSinJS,
+  CSSModules,
+  InlineStyle,
+  TailwindCss,
+  VanilaCSS,
+} from './samples'
+import { SampleType } from './samples/samples'
 
 export default function App() {
-  return <div>App</div>;
+  const [activeStylingSample, setActiveStylingSample] =
+    useState<SampleType>('Vanila css')
+
+  const stylingSamples = [
+    'Vanila css',
+    'tailwind css',
+    'CSS in JS',
+    'css modules',
+    'inline style',
+  ] as SampleType[]
+
+  const handleChangeStylingSample = (sample: SampleType) =>
+    setActiveStylingSample(sample)
+
+  const sampleJSXSwitcher = (sample: SampleType) => {
+    return {
+      'Vanila css': <VanilaCSS />,
+      'tailwind css': <TailwindCss />,
+      'CSS in JS': <CSSinJS />,
+      'css modules': <CSSModules />,
+      'inline style': <InlineStyle />,
+    }[sample]
+  }
+
+  return (
+    <>
+      <div>
+        {stylingSamples.map((sample) => (
+          <button
+            key={sample}
+            onClick={() => handleChangeStylingSample(sample)}
+          >
+            {sample}
+          </button>
+        ))}
+        <div className="p-3 bg-black">
+          {sampleJSXSwitcher(activeStylingSample)}
+        </div>
+      </div>
+    </>
+  )
 }
