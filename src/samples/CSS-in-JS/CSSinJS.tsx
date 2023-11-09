3eda2e5d394660187a4b78d769cb4edfdc746bc7
litophoto <ciro17581@gmail.com>
2023-11-09 20:26:50 +0900

    create: からのサンプルを作成

---
+++ src/samples/CSS-in-JS/CSSinJS.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CSSinJS() {
+  return <div>CSSinJS</div>;
+}
---
+++ src/samples/vanila-css/VanilaCSS.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function VanilaCSS() {
+  return <div>VanilaCSS</div>;
+}
