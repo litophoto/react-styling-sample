e99db79adf40f241a2e8fc2cffa1b40f81c279a6
litophoto <ciro17581@gmail.com>
2023-11-09 21:14:09 +0900

    create: Creation sample's index

---
+++ src/samples/index.ts
@@ -0,0 +1,7 @@
+import VanilaCSS from './vanila-css/VanilaCSS'
+import TailwindCss from './tailwind-css/TailwindCss'
+import CSSinJS from './CSS-in-JS/CSSinJS'
+import CSSModules from './css-modules/CSSModules'
+import InlineStyle from './inline-style/InlineStyle'
+
+export { VanilaCSS, TailwindCss, CSSinJS, CSSModules, InlineStyle }
